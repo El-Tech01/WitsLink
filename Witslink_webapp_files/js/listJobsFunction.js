0ecdb694463d5ffd5b891a016434bb78a6552e24
KGAUGELO MAKOFANE <1604352@students.wits.ac.za>
2020-03-16 19:08:14 +0200

    "test for HtmlDisplayJob function with jobs and no jobs available"

--- Witslink_webapp_files/js/listJobsFunction.js
+++ Witslink_webapp_files/js/listJobsFunction.js
@@ -1,17 +1,18 @@
 //var disJobDiv = document.getElementById("jobDiv");
 function HtmlDisplayJob(data) {
-  var htmlString,hold;
+  var htmlString="",hold;
   hold =data.jobs[0].JOB_TITLE;
   if(!hold.localeCompare("false")){
     htmlString = "No available jobs";
   }
   else{
-    for(i = 0; i < data.length; i++){
-      htmlString += "<br><h2>"+data[i].title+"</h2> <h3> Duration: "+data[i].JOB_STATUS+"</h3> <p> Description: "
-      +data[i].JOB_DESC+"</p> <small> Deadline: "+data[i].JOB_DEADLINE+  "</small>";
+    for(i = 0; i < data.jobs.length; i++){
+      htmlString += data.jobs[i].JOB_TITLE+" Duration: "
+      +data.jobs[i].JOB_STATUS+" Description: "
+      +data.jobs[i].JOB_DESC+" Deadline: "
+      +data.jobs[i].JOB_DEADLINE;
     }
   }
   return htmlString;
 }
-//module.exports = HtmlDisplayJob;
 exports.HtmlDisplayJob = HtmlDisplayJob;
--- Witslink_webapp_files/js/listJobsFunction.test.js
+++ Witslink_webapp_files/js/listJobsFunction.test.js
@@ -1,13 +1,15 @@
 const {HtmlDisplayJob} = require("./listJobsFunction");
 
-/*var json= "{"JOB_TITLE":"false",
-"JOB_STATUS": "Part-Time",
-"JOB_DESC":"WHAT WHAT WHAT WHAT WHAT",
-"JOB_DEADLINE":"02-07-2020"}";*/
-
 test("test the json data array from the php request if no job available",()=>{
   var text = '{"jobs":[' +
   '{"JOB_TITLE":"false"}]}';
   obj = JSON.parse(text);
 expect(HtmlDisplayJob(obj)).toBe("No available jobs");
+});
+
+test("test the json data array from the php request with job available",()=>{
+  var text = '{"jobs":[' +
+  '{"JOB_TITLE":"Tutoring","JOB_STATUS":"Part-Time","JOB_DESC":"WHAT WHAT","JOB_DEADLINE":"02-04-2020"}]}';
+  obj = JSON.parse(text);
+expect(HtmlDisplayJob(obj)).toBe("Tutoring Duration: Part-Time Description: WHAT WHAT Deadline: 02-04-2020");
 });

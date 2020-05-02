ebda306c1492bbee7267efe32f5c8ed647523b2e
unknown <1852529@students.wits.ac.za>
2020-05-02 22:08:36 +0200

    Adding files

--- Witslink_files/Hr Login PHP files/HRLogin.php
+++ Witslink_files/Hr Login PHP files/HRLogin.php
@@ -5,13 +5,14 @@ $database= "d1879990";
 $link = mysqli_connect("127.0.0.1", $username, $password, $database);
 
 $deptID= $_REQUEST["deptID"];
-$deptpassword =$_REQUEST["deptpassword"];
+$deptpassword =$_REQUEST["deptPassword"];
 $output=array();
 if($result =mysqli_query($link,"SELECT * FROM DEPARTMENT WHERE DEPT_ID= $deptID")){
 while($row =$result->fetch_assoc()){
   $output[]=$row;
 }
 }
+$dID= $row[0];
 mysqli_close($link);
 if(empty($output)){
 echo json_encode("false");

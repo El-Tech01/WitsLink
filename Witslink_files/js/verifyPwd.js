1731c6a733e3b644e71b3389b1c1237d26665b53
KGAUGELO MAKOFANE <1604352@students.wits.ac.za>
2020-05-04 03:45:13 +0200

    department login working... Posting jobs working

--- Witslink_files/js/verifyPwd.js
+++ Witslink_files/js/verifyPwd.js
@@ -3,25 +3,24 @@ BtnLogin.addEventListener("click",function(){
   var urlRequest = new XMLHttpRequest();
   var user = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  urlRequest.open('GET','http://lamp.ms.wits.ac.za/~s1879990/Login.php/?USERNAME=10001');
+  urlRequest.open('GET',"http://lamp.ms.wits.ac.za/~s1879990/Login.php/?USERNAME="+user+"&PASSWORD="+password);
   urlRequest.onload = function(){
       var respond = JSON.parse(urlRequest.responseText);
-      HtmlDisplayJob(respond);
+      HtmlOutput(respond);
   };
   urlRequest.send();
 });
 
 var display = document.getElementById("Test");
-// list jobs given data array
-//function to be tested
-function HtmlDisplayJob(data) {
+function HtmlOutput(data) {
   var htmlString="",hold="";
   hold=data[0].DEPT_ID;
   if(!hold.localeCompare("false")){
-    htmlString = "<p>password doesn't match</p>";
+    alert("wrong password or username");
+    //htmlString = "<p>"+"password doesnt match"+"</p>";
   }
   else{
-    htmlString= "<p> password matched </p>";
+    htmlString= window.location.assign("AddJob.html");
+    display.insertAdjacentHTML('beforeend',htmlString);
     }
-  display.insertAdjacentHTML('beforeend',htmlString);
 }

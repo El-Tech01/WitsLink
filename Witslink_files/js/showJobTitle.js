bad7573323471eeabc3f610f6b9258373b01cff9
unknown <1852529@students.wits.ac.za>
2020-05-25 12:12:21 +0200

    small changes

--- Witslink_files/js/showJobTitle.js
+++ Witslink_files/js/showJobTitle.js
@@ -1,7 +1,7 @@
 var jtitle = document.getElementById("jobName");
 var jbname = localStorage.getItem("jname");
 window.addEventListener('load',function(){
-    var htmlstr = "<h3>"+jbname+"</h3>"
+    var htmlstr = "<h3>"+"Job Title: "+jbname+"</h3>"
     jtitle.insertAdjacentHTML('beforeend', htmlstr);
 
-})
+});

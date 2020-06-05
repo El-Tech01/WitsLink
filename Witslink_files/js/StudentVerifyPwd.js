358557e58a9c1caf2bc7baf003b686597d87d624
Sandile <61319304+SandileSustain@users.noreply.github.com>
2020-06-05 11:28:52 +0200

    Update StudentVerifyPwd.js

--- Witslink_files/js/StudentVerifyPwd.js
+++ Witslink_files/js/StudentVerifyPwd.js
@@ -1,15 +1,15 @@
 localStorage.clear();
 
 var BtnLogin = document.getElementById("sBtnLogin");
-var user = document.getElementById("username").value;
 
 BtnLogin.addEventListener("click",function(){
   var urlRequest = new XMLHttpRequest();
   var password = document.getElementById("password").value;
+  var user = document.getElementById("username").value;
   urlRequest.open('GET',"http://lamp.ms.wits.ac.za/~s1879990/StudentLogin.php/?studentNo="+user+"&password="+password);
   urlRequest.onload = function(){
       var respond = JSON.parse(urlRequest.responseText);
-      HtmlOutput(respond);
+      HtmlOutput(respond,user);
   };
   urlRequest.send();
 
@@ -17,15 +17,14 @@ BtnLogin.addEventListener("click",function(){
 
 var display = document.getElementById("Test");
 
-function HtmlOutput(data) {
-  var htmlString="",hold="";
+function HtmlOutput(data,stdNo";
   hold=data[0].STUDENT_NO;
   if(!hold.localeCompare("false")){
     Msg['error']("Incorrect Username or Password. Please Try Again.");
     //htmlString = "<p>"+"password doesnt match"+"</p>";
   }
   else{
-    localStorage.setItem('un',user);
+    localStorage.setItem('un',stdNo);
     htmlString = window.location.assign("index.html");
     display.insertAdjacentHTML('beforeend',htmlString);
     }

53046260080fb63baeff5472e35b736da709d6cf
Sandile <61319304+SandileSustain@users.noreply.github.com>
2020-06-05 12:17:52 +0200

    Update verifyPwd.js

--- Witslink_files/js/verifyPwd.js
+++ Witslink_files/js/verifyPwd.js
@@ -2,7 +2,7 @@ var BtnLogin = document.getElementById("BtnLogin");
 BtnLogin.addEventListener("click",function(){
   var urlRequest = new XMLHttpRequest();
   var user = document.getElementById("username").value;
-  localStorage.setItem("un",user);
+  localStorage.setItem("hr",user);
   var password = document.getElementById("password").value;
   urlRequest.open('GET',"http://lamp.ms.wits.ac.za/~s1879990/HRLogin.php/?USERNAME="+user+"&PASSWORD="+password);
   urlRequest.onload = function(){
